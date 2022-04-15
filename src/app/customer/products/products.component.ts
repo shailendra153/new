5221594dbfe58da9f85fc7c92649d5635f5ee086
14shubh <burghateshubham115@gmail.com>
2022-04-15 23:34:27 +0530

    addign add to cart

--- src/app/customer/products/products.component.ts
+++ src/app/customer/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticateService } from 'src/app/service/Authenticate.service';
 
 @Component({
   selector: 'app-products',
@@ -7,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _authenticate:AuthenticateService, private _router:Router) { }
+  addToCart(){
+    if(this._authenticate.checkToken()){
+      alert('added successful');
+    }else{
+      this._router.navigate(['sign-in']);
+    }
+  }
   ngOnInit(): void {
   }
 
