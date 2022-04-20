ff96a1a760b134576830fb4a358ef5b87aa171a2
Shubham Burghate <84134453+14shubh@users.noreply.github.com>
2022-04-21 00:50:38 +0530

    Update cart.service.ts

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  
 
-  constructor() { }
+  AddToCartUrl:any = "https://artifical.herokuapp.com/cart/add-product"
+  ViewCartUrl:any = "https://artifical.herokuapp.com/cart/view-cart";
+
+  constructor(private _http:HttpClient) { }
+
+  public AddProductInCart(productID: string, userID: any) {
+    let jtwToken = localStorage.getItem('jwt_token')
+   return this._http.post<any>(this.AddToCartUrl,{productId:productID, userId:userID});
+  }
+
+  public FetchCart(UserID:any) {
+   return this._http.post<any>(this.ViewCartUrl,{userId:UserID});
+  }
 }
